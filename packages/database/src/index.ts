450fd3d7f0efe8758b2735db57c9a9f92a798c9c
JayBhende05 <bhendejay5.bms@gmail.com>
2026-04-17 12:22:19 +0530

    REmoved .genreated

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,2 +1,2 @@
 export * from "./client.js"; // exports instance of prisma
-export * from "../generated/prisma/client.js"; // exports generated types from prisma
+// export * from "../generated/prisma/client.js"; // exports generated types from prisma
